737af2215f54437653a6c1275067dff990781674
lakDew <dewangan.lakshya@gmail.com>
2022-12-11 22:20:29 +0530

    second

--- src/Page/home/index.js
+++ src/Page/home/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputSeat from "../../component/input";
 import Reservation from "../../component/Reservation";
 import {
